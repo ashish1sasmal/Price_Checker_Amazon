3a2fdd2229330af6e464102c0f93f985849839cc
ashish1sasmal <ashishsasmal1@gmail.com>
2020-02-02 20:52:17 +0530

    heroku

--- pricehawk/settings.py
+++ pricehawk/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 django_heroku.settings(locals())
 LOGIN_URL='login'
